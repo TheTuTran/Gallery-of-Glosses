d58b36dcccc18dc55f3085ad4dde2ca2fd2a0aa5
TheTuTran <131922588+TheTuTran@users.noreply.github.com>
2023-06-22 12:27:35 -0500

    More code overhaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'primary': 'hsl(215, 35%, 50%)',
-                'accent': 'hsl(12.75, 80%, 40%)',
-                'lightGrey': 'hsl(218, 14%, 85%)',
-                'grey': 'hsl(231, 5%, 48%)',
-                'darkGrey': 'hsl(216, 4%, 26%)',
-                'gold': 'hsl(38, 49%, 43%)',
-                'error': 'hsl(0, 64%, 53%)',
-                'success': 'hsl(113, 81%, 41%)',
-                'bg-color': 'hsl(0, 0%, 100%)',
-                'bg-secondary-color': 'hsl(240, 14%, 96%)',
-            },
-            maxWidth: {
-                'max': '120rem',
-            }
-        },
-     },
-    plugins: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
 }
